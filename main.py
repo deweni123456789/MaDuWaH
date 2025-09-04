ff424dba9b9cdc3404104e487d351f512f6d302a
deweni123456789 <schoford91@gmail.com>
2025-09-04 21:38:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from pyrogram import Client
+from pyrogram.errors import FloodWait
 from modules.yt_downloader import register_youtube
 from modules.song_video import register_song_video
+import asyncio
 
-# ------------------------------
-# CONFIG
-# ------------------------------
-API_ID = 5047271         # Telegram API ID
-API_HASH = "047d9ed308172e637d4265e1d9ef0c27"    # Telegram API HASH
-BOT_TOKEN = "7896090354:AAHC8cSQjnGRhJcPFfciVdyKIqABUkZE1mQ"  # Telegram Bot Token
-# ------------------------------
+API_ID = 5047271
+API_HASH = "047d9ed308172e637d4265e1d9ef0c27"
+BOT_TOKEN = "7896090354:AAHC8cSQjnGRhJcPFfciVdyKIqABUkZE1mQ"
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Register the YouTube downloader module
 register_youtube(app)
 register_song_video(app)
 
-print("🤖 Bot is running...")
-app.run()
+async def start_bot():
+    while True:
+        try:
+            print("🤖 Bot is running...")
+            await app.start()
+            await app.idle()
+            break  # exit loop when stopped normally
+        except FloodWait as e:
+            print(f"[FLOOD_WAIT] Telegram asked to wait {e.x} seconds. Sleeping...")
+            await asyncio.sleep(e.x)
+
+if __name__ == "__main__":
+    asyncio.run(start_bot())
